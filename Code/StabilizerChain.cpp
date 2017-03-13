5a76808d211910b32034c2912d965acca891bcc1
Spencer Parkin <A6GG2ZZ@usac.mmm.com>
2017-03-13 09:38:04 -0600

    comments

--- Code/StabilizerChain.cpp
+++ Code/StabilizerChain.cpp
@@ -22,12 +22,9 @@ bool StabilizerChainGroup::Generate( uint stablePoint, uint maxStablePoint, Word
 	// TODO: Use the orbit-stabilizer theorem and Schreier's lemma.
 	//       Use the basic orbit-algorithm.  Make sure all transversals contain the identity element.
 	//       See: http://www.math.colostate.edu/~hulpke/talks/decomp.pdf
-	//       In that PDF, we find that we need only iterate over the generators to find all orbits.
-	//       This works, because every element is a product of the generators, and we keep track of
-	//       every point we reach in a list that we continually run through all generators.  We need
-	//       to track the chain of steps we take from point to point to point so that we can get the
-	//       transversal element as a single permutation that takes us from the original point to the
-	//       final point.  Keep a parent pointer with each orbit point so that we can construct this.
+	//       This is a BFS of the Caylay graph along generator edges that advances the frontier
+	//       only if a new point in the orbit is encountered.  It can terminate when the queue
+	//       is empty and be sure it found the entire orbit.
 
 	return false;
 }
