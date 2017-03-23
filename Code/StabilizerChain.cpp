0ef549dab10d7aab5939b30912db3929e8443b28
Spencer Parkin <stparkin@mmm.com>
2017-03-23 10:43:37 -0600

    first stab at schreier-sims alg

--- Code/StabilizerChain.cpp
+++ Code/StabilizerChain.cpp
@@ -13,10 +13,14 @@ StabilizerChain::StabilizerChain( void )
 }
 
 // This is an attempt to impliment the Schreier-Sims algorithm.
-bool StabilizerChain::Generate( const PermutationSet& generatorSet )
+bool StabilizerChain::Generate( const PermutationSet& generatorSet, const UintArray& baseArray )
 {
+	this->baseArray.clear();
+	for( uint i = 0; i < baseArray.size(); i++ )
+		this->baseArray.push_back( baseArray[i] );
+
 	delete group;
-	group = new Group();
+	group = new Group( this, 0 );
 
 	for( PermutationSet::iterator iter = generatorSet.begin(); iter != generatorSet.end(); iter++ )
 	{
@@ -25,40 +29,199 @@ bool StabilizerChain::Generate( const PermutationSet& generatorSet )
 			return false;
 	}
 
+	Group* subGroup = group;
+	while( subGroup )
+	{
+		delete subGroup->rootNode;
+		subGroup->rootNode = nullptr;
+		subGroup = subGroup->subGroup;
+	}
+
 	return true;
 }
 
-StabilizerChain::Group::Group( void )
+StabilizerChain::Group::Group( StabilizerChain* stabChain, uint stabilizerOffset )
 {
+	this->stabChain = stabChain;
+	this->stabilizerOffset = stabilizerOffset;
 	subGroup = nullptr;
-	stabilizerPoint = -1;
+	rootNode = nullptr;
 }
 
 /*virtual*/ StabilizerChain::Group::~Group( void )
 {
 	delete subGroup;
+	delete rootNode;
 }
 
 bool StabilizerChain::Group::Extend( const Permutation& generator )
 {
-	// TODO: Add the given generator to our group if it is not already a member of the group.
-	//       If it is a member, we can ignore it.  If it is not, then we add the generator,
-	//       find new orbits/transversal elements, then use the Schreier lemma to make new
-	//       generators for our subgroup and recursively extend the subgroup.  We want to make
-	//       sure that when we're done, we've modified the chain in a way that it is always
-	//       a complete chain for the main group of the chain.
-	//
-	//       There are some complications that will arrise.  As soon as we add a generator to
-	//       a group, making it bigger, its subgroup's transversal set may become entirely invalid.
-	//       We have to keep in mind what sub-chain of our chain is valid/complete at every given point.
+	if( IsMember( generator ) )
+		return true;
+
+	generatorSet.insert( generator );
+
+	PermutationSet singletonSet;
+	singletonSet.insert( generator );
+
+	bool fresh = false;
+	if( !rootNode )
+	{
+		// The root of the orbit tree must be the identity to satisfy a requirement of Schreier's lemma.
+		Permutation identity;
+		transversalSet.insert( identity );
+		PermutationSet::iterator iter = transversalSet.find( identity );
+		rootNode = new OrbitNode( &( *iter ) );
+		fresh = true;
+	}
+
+	if( !rootNode->Grow( this, generatorSet, singletonSet, fresh ) )
+		return false;
+
+	for( PermutationSet::iterator genIter = generatorSet.begin(); genIter != generatorSet.end(); genIter++ )
+	{
+		const Permutation& generator = *genIter;
+
+		for( PermutationSet::iterator transIter = transversalSet.begin(); transIter != transversalSet.end(); transIter++ )
+		{
+			const Permutation& cosetRepresentative = *transIter;
+
+			Permutation product;
+			product.Multiply( cosetRepresentative, generator );
+
+			PermutationSet::iterator iter = FindCoset( product );
+			if( iter == transversalSet.end() )
+				return false;		// Something went wrong with our math!
+
+			Permutation invCosetRepresentative;
+			invCosetRepresentative.SetInverse( *iter );
+
+			Permutation schreierGenerator;
+			schreierGenerator.Multiply( product, invCosetRepresentative );
+
+			if( !schreierGenerator.IsIdentity() )
+			{
+				if( stabilizerOffset >= stabChain->baseArray.size() )
+					return false;
+
+				if( !subGroup )
+					subGroup = new Group( stabChain, stabilizerOffset + 1 );
+
+				if( !subGroup->Extend( schreierGenerator ) )
+					return false;
+			}
+		}
+	}
 
 	return true;
+}
+
+PermutationSet::iterator StabilizerChain::Group::FindCoset( const Permutation& permutation )
+{
+	uint stabilizerPoint = stabChain->baseArray[ stabilizerOffset ];
+
+	Permutation invPermutation;
+	invPermutation.SetInverse( permutation );
+
+	PermutationSet::iterator iter = transversalSet.begin();
+	while( iter != transversalSet.end() )
+	{
+		const Permutation& cosetRepresentative = *iter;
+
+		Permutation product;
+		product.Multiply( cosetRepresentative, invPermutation );
+
+		uint point = product.Evaluate( stabilizerPoint );
+		if( point == stabilizerPoint )
+			break;
+
+		iter++;
+	}
+
+	return iter;
 }
 
 // Assuming that the stabilizer chain rooted as this node is valid, tell
 // us if the given permutation element is a member of this group.
 bool StabilizerChain::Group::IsMember( const Permutation& permutation ) const
 {
+	Permutation invPermutation;
+	return FactorInverse( permutation, invPermutation );
+}
+
+bool StabilizerChain::Group::FactorInverse( const Permutation& permutation, Permutation& invPermutation ) const
+{
+	if( !subGroup )
+		return true;
+
+	uint stabilizerPoint = stabChain->baseArray[ stabilizerOffset ];
+	if( permutation.Evaluate( stabilizerPoint ) == stabilizerPoint )
+		return subGroup->FactorInverse( permutation, invPermutation );
+
+	PermutationSet::iterator iter = const_cast< Group* >( this )->FindCoset( permutation );
+	if( iter == transversalSet.end() )
+		return false;
+
+	const Permutation& cosetRepresentative = *iter;
+
+	Permutation invCosetRepresentative;
+	cosetRepresentative.GetInverse( invCosetRepresentative );
+
+	Permutation product;
+	product.Multiply( permutation, invCosetRepresentative );
+
+	invPermutation.MultiplyOnRight( invCosetRepresentative );
+
+	return FactorInverse( product, invPermutation );
+}
+
+StabilizerChain::OrbitNode::OrbitNode( const Permutation* cosetRepresentative )
+{
+	this->cosetRepresentative = cosetRepresentative;
+}
+
+StabilizerChain::OrbitNode::~OrbitNode( void )
+{
+	for( uint i = 0; i < adjacentNodeArray.size(); i++ )
+		delete adjacentNodeArray[i];
+}
+
+bool StabilizerChain::OrbitNode::Grow( Group* group, const PermutationSet& generatorSet, const PermutationSet& singletonSet, bool fresh )
+{
+	uint stabilizerPoint = group->stabChain->baseArray[ group->stabilizerOffset ];
+
+	const PermutationSet* permutationSet = &generatorSet;
+	if( !fresh )
+		permutationSet = &singletonSet;
+
+	uint nonFreshSize = adjacentNodeArray.size();
+
+	for( PermutationSet::const_iterator iter = permutationSet->cbegin(); iter != permutationSet->cend(); iter++ )
+	{
+		const Permutation& generator = *iter;
+
+		Permutation permutation;
+		permutation.Multiply( *cosetRepresentative, generator );
+
+		uint point = permutation.Evaluate( stabilizerPoint );
+
+		if( !group->orbitSet.IsMember( point ) )
+		{
+			group->orbitSet.AddMember( point );
+
+			group->transversalSet.insert( permutation );
+			PermutationSet::iterator iter = group->transversalSet.find( permutation );
+
+			OrbitNode* orbitNode = new OrbitNode( &( *iter ) );
+			adjacentNodeArray.push_back( orbitNode );
+			if( !orbitNode->Grow( group, generatorSet, singletonSet, true ) )
+				return false;
+		}
+	}
+
+	for( uint i = 0; i < nonFreshSize; i++ )
+		adjacentNodeArray[i]->Grow( group, generatorSet, singletonSet, false );
+
 	return true;
 }
 
--- Code/StabilizerChain.h
+++ Code/StabilizerChain.h
@@ -3,6 +3,10 @@
 #pragma once
 
 #include "Permutation.h"
+#include "NaturalNumberSet.h"
+#include <vector>
+
+typedef std::vector< uint > UintArray;
 
 class StabilizerChain
 {
@@ -11,25 +15,49 @@ public:
 	StabilizerChain( void );
 	virtual ~StabilizerChain( void );
 
-	bool Generate( const PermutationSet& generatorSet );
+	bool Generate( const PermutationSet& generatorSet, const UintArray& baseArray );
+
+	class OrbitNode;
+	class Group;
+
+	typedef std::vector< OrbitNode* > OrbitNodeArray;
+
+	class OrbitNode
+	{
+	public:
+
+		OrbitNode( const Permutation* cosetRepresentative );
+		~OrbitNode( void );
+
+		bool Grow( Group* group, const PermutationSet& generatorSet, const PermutationSet& singletonSet, bool fresh );
+
+		const Permutation* cosetRepresentative;
+		OrbitNodeArray adjacentNodeArray;
+	};
 
 	class Group
 	{
 	public:
 
-		Group( void );
+		Group( StabilizerChain* stabChain, uint stabilizerOffset );
 		virtual ~Group( void );
 
 		bool Extend( const Permutation& generator );
 		bool IsMember( const Permutation& permutation ) const;
+		bool FactorInverse( const Permutation& permutation, Permutation& invPermutation ) const;
+		PermutationSet::iterator FindCoset( const Permutation& permutation );
 
-		uint stabilizerPoint;
+		NaturalNumberSet orbitSet;
+		OrbitNode* rootNode;
+		uint stabilizerOffset;
 		PermutationSet generatorSet;
 		PermutationSet transversalSet;
 		Group* subGroup;
+		StabilizerChain* stabChain;
 	};
 
 	Group* group;
+	UintArray baseArray;
 };
 
 // StabilizerChain.h
